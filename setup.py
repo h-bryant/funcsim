36fb837cfc7f8ab7ff0c216c8c4ab3a476e4ca5c
Craig M. Wellington <cmwellington@tamu.edu>
2019-08-28 15:48:07 -0500

    revert deteting packages

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/h-bryant/funcsim",
-    packages=['funcsim']
+    packages=setuptools.find_packages(),
     install_requires=[
         'numpy',
         'pandas',
