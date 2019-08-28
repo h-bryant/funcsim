a116435c74502d16cbc2fe25b89e138625c03aba
Craig M. Wellington <cmwellington@tamu.edu>
2019-08-28 12:59:44 -0500

    created sample pip setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="funcsim",
+    version="0.0.1",
+    author="Henry Bryant",
+    author_email="henry@tamu.edu",
+    description="functional, simple stochastic simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/h-bryant/funcsim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis"
+    ],
+)
