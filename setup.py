ff0c49d8ee52c59013d28e34069db47b65221e2d
Craig M. Wellington <cmwellington@tamu.edu>
2019-08-28 14:01:13 -0500

    add dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/h-bryant/funcsim",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'xarray'
+    ],
+    test_requires=[
+        'scipy',
+        'pytest',
+        'sphinx'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
