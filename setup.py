46091d061e6370184ec1dd28f0df121dbd31937e
henry <henry@tamu.edu>
2019-11-04 09:36:57 -0600

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="funcsim",
-    version="0.0.3",
+    version="0.0.4",
     license="bsd-3-clause",
     author="Henry Bryant",
     author_email="henry@tamu.edu",
