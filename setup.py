3455d504c09f1f38bd4e907cd08261a7e954544f
henry <henry@tamu.edu>
2019-11-04 14:23:23 -0600

    remove deprecated helpers.py; increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="funcsim",
-    version="0.0.4",
+    version="0.0.5",
     license="bsd-3-clause",
     author="Henry Bryant",
     author_email="henry@tamu.edu",
