c989a932492235e2539030bf6ec0ff3bfce20559
Henry Bryant <h-bryant@tamu.edu>
2020-09-07 09:57:06 -0500

    increment version to 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="funcsim",
-    version="0.0.6",
+    version="0.0.7",
     license="bsd-3-clause",
     author="Henry Bryant",
     author_email="henry@tamu.edu",
