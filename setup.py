92c5ec59eddf7d83a9104aaf8f54781abc292908
hlb4tx@gmail.com <hlb4tx@gmail.com>
2025-05-30 10:29:09 -0500

    speicfy optional dependencies

--- setup.py
+++ setup.py
@@ -15,14 +15,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/h-bryant/funcsim",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'copulae>=0.8.0',
-        'numpy>=2.2.5',
-        'pandas>=2.2.3',
-        'plotly>=6.0.1',
-        'scipy>=1.15.3',
+    install_requires=[ 
+        'numpy>=2.2.0',
+        'pandas>=2.2.0',
+        'scipy>=1.15.0',
         'xarray>=2025.4.0'
+        'statsmodels>=0.14.0',
     ],
+    extras_require={
+        "plot": ["plotly>=6.0.0"],
+        "copula": ['copulae>=0.8.0'],
+        "full": ["plotly>=6.0.0", "copulae>=0.8.0"],
+    },
     test_requires=[
         'pytest',
         'sphinx',
